d41e150ee2a6ff748aad680afe2188dfd916a5da
Nikki_Moscow <nikk178@mail.ru>
2017-03-27 20:10:10 +0300

    Add BMSTU_Map v. 0.1
    Add first main screen 📺 and add disdain

---
+++ BMSTU_Map/BMSTU_Map/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMSTU_Map
+//
+//  Created by Никита Курганов on 25.03.17.
+//  Copyright © 2017 Никита Курганов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
