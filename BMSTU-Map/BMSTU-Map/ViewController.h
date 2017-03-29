e400d4c970dfdab7c67b26bf4ca79baffedafe2b
Nikki_Moscow <nikk178@mail.ru>
2017-03-30 00:10:32 +0300

    Add BMSTU_Map v. 0.1
    Добавлен начальный экран приложения с минимальным функционалом

---
+++ BMSTU-Map/BMSTU-Map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BMSTU-Map
+//
+//  Created by Никита Курганов on 29.03.17.
+//  Copyright © 2017 Никита Курганов. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BMSTU-Map/BMSTU-Map/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BMSTU-Map
+//
+//  Created by Никита Курганов on 29.03.17.
+//  Copyright © 2017 Никита Курганов. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
