ada13e8adb4b7cf007e0250574dba549f9b271d5
Nikki_Moscow <nikk178@mail.ru>
2017-04-04 20:47:03 +0300

    Add BMSTU-Map v. 0.2
    Add Realm database

---
+++ BMSTU-Map/BMSTU-Map/Auditories.swift
@@ -0,0 +1,18 @@
+//
+//  Auditories.swift
+//  BMSTU-Map
+//
+//  Created by Никита Курганов on 03.04.17.
+//  Copyright © 2017 Никита Курганов. All rights reserved.
+//
+
+import RealmSwift
+
+class Auditories: Object {
+    
+// Specify properties to ignore (Realm won't persist these)
+    
+//  override static func ignoredProperties() -> [String] {
+//    return []
+//  }
+}
---
+++ BMSTU-Map/BMSTU-Map/NoteRealm.swift
@@ -0,0 +1,15 @@
+//
+//  NoteRealm.swift
+//  BMSTU-Map
+//
+//  Created by Никита Курганов on 03.04.17.
+//  Copyright © 2017 Никита Курганов. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class NoteRealm : Object{
+
+
+}
